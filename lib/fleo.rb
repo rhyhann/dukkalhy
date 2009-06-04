a10b72415a79aab88d41116853d2acd3bb0c7344
hy <hy@eo.(none)>
2009-06-04 19:25:13 +0100

    Single lib fire, à la camping

--- lib/fleo.rb
+++ lib/fleo.rb
@@ -1,39 +1,4 @@
-
-# The main Fleo class
-# This is just a placeholder.
-# Here is how fleo works :
-#  - For each object inside _pages
-#    = If it's a draft
-#      - Just save the name of it in the object with the "draft" state
-#      - Include it in the html report
-#    = Else
-#      - It will see which pageholders contains it, including the self page,
-#        which is _always_ rendered
-#        = How to guess pageholders ?
-#          There are two types of page holders:
-#          - outside - guessed with the directories:
-#            _pages/foo/bar/page will have /, foo and bar as category
-#            placeholders
-#          - inside  - guessed with the attributes:
-#            each field of the tag attribute will be a tag placeholder
-#          A thild one may be added, the special one:
-#          - self - may just contain the object, rendered with the object's
-#            categoly template
-#      - When the placeholders are determined, with their paths:
-#        = Invoke the relative template, with the object, generate it and 
-#          put it in the _site
-#        = How to generate paths ?
-#          It's all about the object's template path, guy !
-# NB: This short introduction has been made for me. If I let this in the 
-# version 1 or more, please contact me so I rewrite this to a more... human
-# explication, n'est-ce pas ?
-#
-#
-
-%w(
-   rubygems ostruct
-   yaml
-  ).each {|lib| (require lib)}
+%w(ostruct yaml).each {|lib| (require lib)}
 
 # Ruby extensions
 class Hash
@@ -45,10 +10,72 @@ class Hash
   end
 end
 
-class String
-  def remove!(pattern)
-    replace (self.sub pattern, '')
+# The program
+module  Fleo
+  
+  # This is the first step of the site generation.
+  # This module should be used with the *folder* or *file* method,
+  # and it will either return an array of all the elements (Array of Openstruct)
+  # or the single element (Openstruct).
+  # Some people would wonder why these methods are so splat. It's because I plan,
+  # in order to take over the world, to add more content sources, in the future.
+  # It's nevertheless not splat enough (TODO)
+  module Parser
+    
+    def self.folder(path)
+      # TODO: subfolder or folder method
+      Dir.glob("#{path}/**/*.*").map! {|f| (file f)}
+    end
+
+    def self.file(path)
+      yaml (File.read path)
+    end
+
+    def self.yaml(content)
+      # The header
+      header  = (YAML.load content).symbolize
+      # The content. We just remove the header
+      content.sub! /.*\-\-\-\n/im, ''
+      parse path, header, content
+    end
+
+    def self.parse(full_path, params, content)
+      # If it's the full path, we remove unneeded path content
+      path = full_path.sub(/.*\/pages/, '/pages').split '/'
+      # The file name parsing
+      regular = /_?([a-zA-Z]*)_?(\d+)-(\d+)-(\d+)_(\w+)\.([a-zA-Z]+)/
+      if path[-1] =~ regular
+        params[:markers], params[:year]     , params[:month]    ,  \
+            params[:day], params[:filetitle], params[:formatter] = \
+            *(path[-1].scan regular)[0]
+        params[:markers] = params[:markers].split '_'
+      else
+        raise ArgumentError, "Bad naming of #{full_path}"
+      end
+      # The file name and pages/ are useless
+      path.delete_at(-1)
+      path.shift
+      # The other params to add
+      params[:folders] = path
+      params[:content] = content
+      
+      OpenStruct.new params
+    end
+  end
+
+
+  module Holder
+
+    # When holding a block, the first argument
+    # is the array of all the pages, the second
+    # is eventually the parent.
+    # The only role of this is to create the template
+    # variables.
+    def self.add(name, parent = nil, &block)
+      @blocks ?
+          @blocks[name] = [block, parent] :
+          @blocks       = {name => [block, parent]}
+    end
+
   end
 end
-
-Dir.glob(File.dirname(__FILE__) + "/fleo/*.rb") {|file| (require file) }
